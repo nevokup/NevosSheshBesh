a9f062d5e74a2370ac7405b6ce4a54ab4826ecc7
nevokup <s67546@haklaiph.org>
2025-12-07 04:53:43 +0200

    init

---
+++ app/src/main/java/com/example/nevos_shesh_besh/shapes/TeleportCircle.java
@@ -0,0 +1,20 @@
+package com.example.nevos_shesh_besh.shapes;
+
+public class TeleportCircle extends CircleShape {
+    private final int originalColor;
+    private final int activeColor;
+    public boolean isActive = false;
+    public TeleportCircle(float x, float y, float radius, int color, int activeColor) {
+        super(x, y, radius, color);
+        this.originalColor = color;
+        this.activeColor = activeColor;
+    }
+
+    public void setActive(boolean active) {
+        this.isActive = active;
+        this.paint.setColor(active ? activeColor : originalColor);
+    }
+
+
+}
+
